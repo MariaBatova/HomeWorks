d84b39a8e4c2b5a20f839310ecebb988f286b844
Маруся Батова <Maria>
2022-10-04 17:55:32 +0300

    Task 6

--- HomeWorks/HomeWorks1/Program.cs
+++ HomeWorks/HomeWorks1/Program.cs
@@ -15,6 +15,7 @@ else
 }
 */
 
+/*
 // Задача 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 Console.Write("Input a first number: ");
 int firstNum = Convert.ToInt32(Console.ReadLine());
@@ -35,3 +36,17 @@ if (max < thirdNum)
 }
 
  Console.WriteLine($"Max number is {max}");
+*/
+
+// Задача 6. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
