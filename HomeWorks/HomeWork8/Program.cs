0f49d6443855389d19e6f0d458743c91ce9610a8
Маруся Батова <Maria>
2022-11-11 11:09:02 +0300

    Home Work 8

---
+++ HomeWorks/HomeWork8/Program.cs
@@ -0,0 +1,337 @@
+﻿/*
+// Задача 54. Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+int[,] CreateRandomArray()
+{
+    Console.Write("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeElements(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, k] > array[i, j])
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, j];
+                    array[i, j] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+int[,] newArray = CreateRandomArray();
+PrintArray(newArray);
+Console.WriteLine();
+int[,] changeArray = ChangeElements(newArray);
+PrintArray(changeArray);
+*/
+
+/*
+// Задача 56. Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+int[,] CreateRandomArray()
+{
+    Console.Write("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+int[] SumElementsRows(int[,] array)
+{
+    int[] sumArray = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i, j];
+            sumArray[i] = sum;
+        }
+    }
+    return sumArray;
+}
+
+void PrintSumArray(int[] array)
+{
+    Console.Write("Sum elements rows: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+            Console.Write(", ");
+    }
+    Console.WriteLine();
+}
+
+int IndexMinSumElements(int[] array)
+{
+    int iMin = 0;
+    int min = array[iMin];
+    for (int i = 1; i < array.Length - 1; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+            iMin = i;
+        }
+    }
+    return iMin;
+}
+
+int[,] newArray = CreateRandomArray();
+PrintArray(newArray);
+Console.WriteLine();
+int[] sumArray = SumElementsRows(newArray);
+PrintSumArray(sumArray);
+int numberRow = (IndexMinSumElements(sumArray) + 1);
+Console.WriteLine($"Row number with the smallest sum of elements- {numberRow}.");
+*/
+
+/*
+// Задача 58. Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+int[,] CreateRandomArray()
+{
+    Console.Write("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+int[,] MatrixPossible(int[,] array1, int[,] array2)
+{
+    var newArray = new int[array1.GetLength(0), array2.GetLength(1)];
+    if (array1.GetLength(0) != array2.GetLength(1))
+    {
+        Console.WriteLine("Matrix product is not possible!!");
+        Console.WriteLine("The number of lines must be the same!");
+        return null;
+    }
+    else
+    {
+        for (int i = 0; i < array1.GetLength(0); i++)
+        {
+            for (int j = 0; j < array2.GetLength(1); j++)
+            {
+                newArray[i, j] = 0;
+                for (int k = 0; k < array2.GetLength(0); k++)
+                {
+                    newArray[i, j] += array1[i, k] * array2[k, j];
+                }
+            }
+        }
+        return newArray;
+    }
+}
+
+int[,] array1 = CreateRandomArray();
+PrintArray(array1);
+int[,] array2 = CreateRandomArray();
+PrintArray(array2);
+int[,] resultArray = MatrixPossible(array1,array2);
+Console.ForegroundColor = ConsoleColor.Green;
+if(resultArray != null) PrintArray(resultArray);
+*/
+
+/*
+// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+int[] PositiveArray()
+{
+    int[] array = Enumerable.Range(10, 90).ToArray();
+    for (int i = 0; i < array.Length; i++)
+    {
+        int j = new Random().Next(0, 90);
+        if (j != i)
+        {
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+    return array;
+}
+
+int[,,] CreateArray(int[] array, int rows, int columns, int depth)
+{
+    int m = 0;
+    int[,,] array3D = new int[rows, columns, depth];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            for (int k = 0; k < depth; k++)
+            {
+                array3D[i, j, k] = array[m];
+                m++;
+            }
+        }
+    }
+    return array3D;
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write(array[i, j, k] + " ");
+                Console.Write($"({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+try
+{
+    Console.Write("Enter the number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter the number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter the depth of the array: ");
+    int depth = Convert.ToInt32(Console.ReadLine());
+    int[] array = PositiveArray();
+    int[,,] array3D = CreateArray(array, rows, columns, depth);
+    PrintArray(array3D);
+}
+catch(Exception)
+{
+    Console.WriteLine("Invalid input!");
+}
+*/
+
+
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+int[,] ArraySpiral(int rows, int columns)
+{
+    int[,] arraySpiral = new int[rows, columns];
+    for (int passage = 0, valueElements = 1; valueElements <= rows * columns; passage++)
+    {
+        for (int i = passage, j = passage; j < columns - passage; j++)
+        {
+            arraySpiral[i, j] = valueElements;
+            valueElements++;
+        }
+        if(valueElements > rows * columns) break;
+        for (int i = passage + 1, j = columns - passage - 1; i < rows - passage; i++)
+        {
+            arraySpiral[i, j] = valueElements;
+            valueElements++;
+        }
+        if(valueElements > rows * columns) break;
+        for (int i = rows - passage - 1, j = columns - passage - 2; j >= passage; j--)
+        {
+            arraySpiral[i, j] = valueElements;
+            valueElements++;
+        }
+        if(valueElements > rows * columns) break;
+        for (int i = rows - passage - 2, j = passage; i > passage; i--)
+        {
+            arraySpiral[i, j] = valueElements;
+            valueElements++;
+        }
+    }
+    return arraySpiral;
+}
+
+void PrintArray(int[,] array, int fillZeros = 2)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j].ToString($"D{fillZeros}") + " "); 
+        Console.WriteLine();
+    }
+}
+
+try
+{
+    Console.Write("Enter the number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter the number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+
+    int[,] arraySpiral = ArraySpiral(rows, columns);
+    PrintArray(arraySpiral);
+}
+catch(Exception)
+{
+    Console.WriteLine("Invalid input!");
+}
+
